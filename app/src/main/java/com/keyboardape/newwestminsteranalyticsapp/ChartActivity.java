aaa286656a26dfc0c2fe7ce6bad7cb2df6f97f17
Tony So <tony.so.1116@gmail.com>
2017-11-10 22:42:34 -0800

    Moved the Chart to display the middle Changed alot of the chartActivity to display 1 bar instead of having it side by side

--- app/src/main/java/com/keyboardape/newwestminsteranalyticsapp/ChartActivity.java
+++ app/src/main/java/com/keyboardape/newwestminsteranalyticsapp/ChartActivity.java
@@ -2,23 +2,22 @@
 
 package com.keyboardape.newwestminsteranalyticsapp;
 
-import android.app.Activity;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.widget.Toast;
 
 import com.github.mikephil.charting.charts.BarChart;
-import com.github.mikephil.charting.components.XAxis;
-import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
 import com.keyboardape.newwestminsteranalyticsapp.db.DBHelper;
 
 import java.util.ArrayList;
@@ -33,8 +32,6 @@ public class ChartActivity extends AppCompatActivity {
 
     BarChart barChart;
     float barWidth;
-    float barSpace;
-    float groupSpace;
     private SQLiteDatabase db;
     private Cursor cursor;
     Map<String,Float> graphCount = new HashMap<String,Float>();
@@ -91,37 +88,45 @@ public class ChartActivity extends AppCompatActivity {
     {
         // Declarations
         barChart = (BarChart) findViewById(R.id.bargraph);
-        barWidth = 0.4f;
-        barSpace = 0f;
-        groupSpace = 0.4f;
+        barWidth = 0.9f;
 
-        //ArrayList of fields
-        ArrayList labels = new ArrayList();
-        labels.add("1986");
-        labels.add("1991");
-        labels.add("1996");
-        labels.add("2001");
+        // Set animation to make it cool
+        barChart.animateY(3000);
 
-        // To store y-Axis of the data
-        ArrayList<BarEntry> dummyEntries = new ArrayList<>();
+        // X-Axis settings
+        barChart.getXAxis().setDrawGridLines(false);
+        barChart.getXAxis().setDrawLabels(false);
 
-        // Really ugly sort code
+        // Y-Axis settings
+        barChart.getAxisLeft().setAxisMinimum(0); // Starts at 0
+        barChart.getAxisRight().setAxisMinimum(0); // Starts at 0
+
+        //Make the description empty
+        Description description = new Description();
+        description.setText("");
+        barChart.setDescription(description);
+
+        // To store the data in somehow
+        ArrayList<BarEntry> barEntries = new ArrayList<>();
+
+        // Really ugly sort code to sort the hashmap to values
         Set<Map.Entry<String, Float>> set = graphCount.entrySet();
         List<Map.Entry<String, Float>> list = new ArrayList<Map.Entry<String, Float>>(set);
         Collections.sort( list, new Comparator<Map.Entry<String, Float>>()
-        {
-            public int compare( Map.Entry<String, Float> o1, Map.Entry<String, Float> o2 )
             {
-                return (o2.getValue()).compareTo( o1.getValue() );
+                public int compare( Map.Entry<String, Float> o1, Map.Entry<String, Float> o2 )
+                {
+                    return (o2.getValue()).compareTo( o1.getValue() );
+                }
             }
-        } );
+        );
 
+        //Help gets the top 10 most popular businesses
         int i = 1;
         int topTen = 0;
-
         for (Map.Entry<String, Float> entry:list) {
             if (entry.getValue() > 1 && topTen < 10) {
-                dummyEntries.add(new BarEntry(i++, entry.getValue())); //Entries must be floats
+                barEntries.add(new BarEntry(i++, entry.getValue())); //Entries must be floats
                 barChart.notifyDataSetChanged();
                 barChart.invalidate();
                 System.out.println(i + " " + entry.getValue());
@@ -129,46 +134,30 @@ public class ChartActivity extends AppCompatActivity {
             }
         }
 
-        ArrayList<BarEntry> dummyEntries2 = new ArrayList<>();
-        dummyEntries2.add(new BarEntry(1,26f));
-        dummyEntries2.add(new BarEntry(2,58f));
-        dummyEntries2.add(new BarEntry(3,13f));
-        dummyEntries2.add(new BarEntry(4,48f));
-        dummyEntries2.add(new BarEntry(5,48f));
-
         // Is needed so the data can be outputted
-        BarDataSet dummySet1 = new BarDataSet(dummyEntries,"Dummy Entries 1");
-        BarDataSet dummySet2 = new BarDataSet(dummyEntries2,"Dummy Entries 2");
+        BarDataSet barDataSet = new BarDataSet(barEntries,"Most Popular Businesses");
+        barDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
 
-        // Changes the color of the bar
-        dummySet1.setColor(Color.CYAN);
-        dummySet2.setColor(Color.BLACK);
+        BarData data = new BarData(barDataSet);
 
-        BarData dummyData = new BarData(dummySet1,dummySet2);
+        barChart.setData(data);
 
-        barChart.setData(dummyData); // Sets the data and list into the chart
-
-        barChart.getBarData().setBarWidth(barWidth); // Sets the width of the chart
-        barChart.getXAxis().setAxisMinimum(0); // Shows the mininum of the chart
-
-        // Shows the maximum amount of the width and height, 4 because there are 4 entries
-        // Sets X-Axis settings
-        barChart.getXAxis().setAxisMaximum(0 + barChart.getBarData().getGroupWidth(groupSpace, barSpace) * i);
-        barChart.getXAxis().setDrawGridLines(false);
-        barChart.groupBars(0, groupSpace, barSpace); // Helps splits the bars
-        barChart.invalidate(); // don't know what this does
-
-
+        // Settings, Needs to be declared at the very bottom
+        data.setBarWidth(barWidth);
         barChart.getData().setHighlightEnabled(false); // Prevents tapping on the bars
         barChart.setTouchEnabled(true);
         barChart.setDragEnabled(true);
         barChart.setScaleEnabled(true);
-        barChart.setFitBars(true);
+        barChart.setFitBars(true); // makes the x-axis fit exactly all bars
         barChart.setDoubleTapToZoomEnabled(true);
+        barChart.invalidate(); // refreshes (redraws the chart)
 
-        // Y-Axis settings
-        barChart.getAxisLeft().setAxisMinimum(0); // Starts at 0
-        barChart.getAxisRight().setAxisMinimum(0); // Starts at 0
+        testLegend(barChart);
+    }
 
+    private void testLegend(BarChart barChart) {
+        Legend l = barChart.getLegend();
+        //Hides the legend for now
+        l.setEnabled(false);
     }
 }
